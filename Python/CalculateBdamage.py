8624327ba70568c9a007c4d94898111c37580a10
td93 <tomdixon93@gmail.com>
2015-11-12 13:09:45 +0000

    working downlaod pdb script

--- Python/CalculateBdamage.py
+++ Python/CalculateBdamage.py
@@ -6,6 +6,8 @@ def CalculateBdamage(pathToPDB, PDT=14, binSize=10, createUnitCellPDB=0, createT
     #import packages required for running the program
     import time #for 
     import sys #for terminating script when encountering errors
+    import urllib2 #for dealing with URL stuff
+    import os #for operating system usability
     #Input: the file path to the pdb for which you want to calculate B-damage factors, the 'Packing Density Threshold' (Angstroms) and bin size
     start = time.time()
     startIndex = time.gmtime()
@@ -29,9 +31,9 @@ def CalculateBdamage(pathToPDB, PDT=14, binSize=10, createUnitCellPDB=0, createT
     if PDT == 14:
         print 'No packing density threshold supplied, using default value of 14 Angstroms'
     else:
-        print 'Packing density threshold defined by user at %s Angstroms' % PDT
+        print 'Packing density threshold defined by user at %s Angstroms\n' % PDT
     if binSize == 10:
-        print 'No bin size supplied, using default value of 10'
+        print 'No bin size supplied, using default value of 10\n'
     else:
         print 'Bin size defined by user as %s\n' % binSize
     print '********** End of Input Section ********************************'
@@ -49,21 +51,45 @@ def CalculateBdamage(pathToPDB, PDT=14, binSize=10, createUnitCellPDB=0, createT
     if len(pathToPDB) == 4:
         print 'PDB code supplied'
         #convert PDB accession code to UPPERCASE
-        pathToPDB = pathToPDB.ascii_uppercase
-        #create URL from which to download .pdb file
-        urlText = 'http://www.rcsb.org/pdb/files/%s.pdb' % pathToPDB
-    else:
-        if pathToPDB[-4:] == '.pdb':
-            print 'Filepath to .pdb file supplied'
+        PDBcode = pathToPDB.upper()
+        PDBdirectory = 'Logfiles/%s/' % PDBcode
+        pathToPDB = '%s%s.pdb' % (PDBdirectory, PDBcode)
+        if os.path.isfile(pathToPDB):
+            #inform user that file already exists
+            print 'PDB file already exists locally\n'
         else:
-            sys.exit('Error 01: Suppplied filepath to PDB is not a .pdb file')
+            #create URL from which to download .pdb file
+            urlText = 'http://www.rcsb.org/pdb/files/%s.pdb' % PDBcode
+            #check for valid PDB code, returning error message if not
+#            if urllib2.URLError:
+#                sys.exit ('Error 03: PDB code does not exist')
+            #downlaod PDB file and save local copy
+            os.makedirs(PDBdirectory)
+            origPDB = urllib2.urlopen(urlText)
+            #inform user of the URL used to download PDB file
+            print 'Downloaded PDB file from %s' % urlText
+            #write local file containing the downloaded content
+            localFile = open(pathToPDB, 'w') 
+            localFile.write(origPDB.read())
+            print 'PDB file saved to %s\n' % pathToPDB
+            #close local file to free up memory
+            localFile.close()
+    else:
+        #check supplied filepath is a pdb file, returning error message if not
+        if pathToPDB[-4:] == '.pdb':
+            print 'Filepath to .pdb file supplied\n'
+        else:
+            sys.exit('Error 01: Supplied filepath to PDB is not a .pdb file')
+        #check supplied filepath exists, returning error message if not
+        if not os.path.exists(pathToPDB):
+            sys.exit ('Error 02: Supplied filepath does not exist')
     print '********** End of Process PDB Section **************************'
     print '****************************************************************'
     print '\n'
     time.sleep(2)
     #inform the user of the time elapsed while the program was run
     runtime = time.time() - start
-    print 'Program run in %f seconds\n' % runtime
+    print 'Program run in %.3f seconds\n' % runtime
 #    print 'Total time taken for program to run was %.0f minutes and %.0f seconds.\n\n' % floor(timetaken/60) % rem(timetaken/60)    
 
 #end
