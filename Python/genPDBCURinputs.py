b6880f86a1d5294f3938bb58f6ea3e4fd8f52692
td93 <tomdixon93@gmail.com>
2015-11-17 15:00:56 +0000

    latest additions

--- Python/genPDBCURinputs.py
+++ Python/genPDBCURinputs.py
@@ -1,5 +1,17 @@
 #Copyright Thomas Dixon 2015
-def genPDBCURinputs(pathToPDB, PDBdirectory):
+def genPDBCURinputs(pathToPDB, PDBdirectory, delhydrogen):
+    #import os for OS usability
+    import os
+    #create path to PDBCUR input file
+    PDBCURinputFile = '%s/PDBCURinput.txt' % PDBdirectory 
+    #check if an input file has already been created
+    if os.path.exist(PDBCURinputFile):
+        #inform user file already exists
+        print 'Input file for PDBCUR already exist at %s/PDBCURinputFile.txt' % PDBdirectory
+        return
+        
+    #open a text file for writing
     
-    inputs.filename = 'PDBCURinputFile.txt'
-    inputs.delhydrogen
+    
+    
+#end
