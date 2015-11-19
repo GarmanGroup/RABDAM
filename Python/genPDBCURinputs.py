f88480f9f0ad134db7ab3484e3ad4fba5aa1a67f
td93 <tomdixon93@gmail.com>
2015-11-19 12:02:07 +0000

    Finished working generate PDBCUR inputs module

--- Python/genPDBCURinputs.py
+++ Python/genPDBCURinputs.py
@@ -5,13 +5,27 @@ def genPDBCURinputs(pathToPDB, PDBdirectory, delhydrogen):
     #create path to PDBCUR input file
     PDBCURinputFile = '%s/PDBCURinput.txt' % PDBdirectory 
     #check if an input file has already been created
-    if os.path.exist(PDBCURinputFile):
+    if os.path.exists(PDBCURinputFile):
         #inform user file already exists
-        print 'Input file for PDBCUR already exist at %s/PDBCURinputFile.txt' % PDBdirectory
-        return
-        
+        print 'Input file for PDBCUR already exist at %s' % PDBCURinputFile
+        #exit method if file exists
+        return    
     #open a text file for writing
-    
-    
-    
+    print 'Creating input file for PDBCUR at %s' % PDBCURinputFile
+    #write input keywords to file for use with PDBCUR
+    with open(PDBCURinputFile,'w') as f:
+        #only write input line delhydrogen if not specified otherwise by user
+        if delhydrogen == 1:
+            #delhydrogen keyword removes all hydrogen atoms from PDB
+            f.write('delhydrogen\n')
+        #cutocc keyword removes all atoms with occupancy of 0 from PDB
+        f.write('cutocc\n')
+        #mostprob keyword only keeps atoms from conformations with the highest occupancy
+        #if equal occupancies then only one is retained (A)
+        f.write('mostprob\n')
+        #noanisou keyword removes all ANISOU information from PDB
+        f.write('noanisou\n')
+        #genunit keyword generates a unit cell
+        f.write('genunit\n')
+        f.close
 #end
