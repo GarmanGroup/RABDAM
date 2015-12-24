5716ba42c0fbf05153232c4464658e2340e708e5
td93 <tomdixon93@gmail.com>
2015-12-24 22:34:56 +0000

    latest edition

--- Python/atomCheck.py
+++ Python/atomCheck.py
@@ -11,12 +11,12 @@ def convertParams(params, margin):
 #end convertParams
 
 def isInXYZparams(atomXYZ, params):
-    x = atomXYZ[0][0]
-    y = atomXYZ[1][0]
-    z = atomXYZ[2][0]
-    if params[0] < x < params[1]:
-        if params[2] < y < params[3]:
-            if params[4] < z < params[5]:
+    x = float(atomXYZ[0][0])
+    y = float(atomXYZ[1][0])
+    z = float(atomXYZ[2][0])
+    if float(params[0]) < x < float(params[1]):
+        if float(params[2]) < y < float(params[3]):
+            if float(params[4]) < z < float(params[5]):
                 return True
     return False
 #end isInParams
