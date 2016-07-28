52f745e7864bf64e0af53d5356eb5bcd4bc02294
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2016-07-28 14:51:03 +0100

    formatting changes. Nothing of any substance

--- CalculateBdamage/calcPackingDensityLoop.m
+++ CalculateBdamage/calcPackingDensityLoop.m
@@ -5,20 +5,20 @@ function packingDensityArray = calcPackingDensityLoop(xyzPDBAtom,xyzSurroundingA
 packingDensityArray = zeros(numberOfAtomsInPDB,1);
 
 %Loop through each atom
-for eachPDBAtom = 1 : numberOfAtomsInPDB 
-    
+for eachPDBAtom = 1 : numberOfAtomsInPDB
+
     %Set the atom packing density to zero. Here the packing density is
     %defined by the atomic contact number with contact being defined as any
     %atom within the packingDensityThreshold distance of it.
     atomPackingDensity = 0;
-    
+
     %Loop through each of the surrounding atoms that can potentially
     %contribute to the packing density
     for eachSurroundingAtom = 1 : numberOfSurroundingAtoms
-        
+
         %Calculate distance between the surrounding atom and the pdb atom
         distanceBetweenAtoms = sqrt((xyzPDBAtom(eachPDBAtom,1) - xyzSurroundingAtom(eachSurroundingAtom,1))^2 + (xyzPDBAtom(eachPDBAtom,2) - xyzSurroundingAtom(eachSurroundingAtom,2))^2 + (xyzPDBAtom(eachPDBAtom,3) - xyzSurroundingAtom(eachSurroundingAtom,3))^2);
-        
+
         %Check if the distance is below the packing density threshold
         if distanceBetweenAtoms <= packingDensityThreshold
             %if so then increase the packing density counter by 1
@@ -26,9 +26,8 @@ for eachPDBAtom = 1 : numberOfAtomsInPDB
         end
     end
     %store the packing density of the atom in the cell array
-    packingDensityArray(eachPDBAtom) = atomPackingDensity;   
+    packingDensityArray(eachPDBAtom) = atomPackingDensity;
 end
 
 
 end
-
