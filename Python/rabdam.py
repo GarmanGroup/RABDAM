02f7912d10d82a3a652276eb6b5e141c1bbb62db
Kathryn Shelley <quee2849@bioch.ox.ac.uk>
2016-08-05 14:55:46 +0100

    Changed name at Elspeth's request

--- Python/rabdam.py
+++ Python/rabdam.py
@@ -3,26 +3,26 @@
 # Script to calculate B-damage for protein atoms
 import sys
 sys.path.insert(0, './Subroutines')
-from CalculateBdamage import cambda
+from CalculateBdamage import rabdam
 
 # read the filename from the CL input
 fileCont = open(sys.argv[1], 'r')
 # write the string of the function arguments
 functionArgs = fileCont.read()
 # remove all whitespace from the inputs
-functionArgs = functionArgs.replace(" ", "")
-functionArgs = functionArgs.replace("\n", "")
-functionArgs = functionArgs.replace("\r", "")
+functionArgs = functionArgs.replace(' ', '')
+functionArgs = functionArgs.replace('\n', '')
+functionArgs = functionArgs.replace('\r', '')
 # split the input file into its components
-splitArgs = functionArgs.split(",")
-# parse the argument string and assign values to CaMBDa inputs
+splitArgs = functionArgs.split(',')
+# parse the argument string and assign values to RABDAM inputs
 pathToPDB = splitArgs[0]
 # initialise the argument defaults
 pdtVal = int(14)
 binVal = int(10)
 addAtomsList = []
 removeAtomsList = []
-thresholdVal = float(0.05)
+thresholdVal = float(0.02)
 aucVal = False
 taVal = False
 
@@ -32,20 +32,25 @@ for x in xrange(1, len(splitArgs)):
         pdtVal = float(pdtArg[len(pdtArg)-1])
         if pdtVal == int(pdtVal):
             pdtVal = int(pdtVal)
+        break
     elif splitArgs[x][0:7] == 'binSize':
         binArg = splitArgs[x].split('=')
         binVal = int(binArg[len(binArg)-1])
+        break
     elif splitArgs[x][0:8] == 'addAtoms':
         addAtomsArg = splitArgs[x].split('=')
         addAtomsStr = str(addAtomsArg[len(addAtomsArg) - 1])
         addAtomsList = addAtomsStr.split(';')
+        break
     elif splitArgs[x][0:11] == 'removeAtoms':
         removeAtomsArg = splitArgs[x].split('=')
         removeAtomsStr = str(removeAtomsArg[len(removeAtomsArg) - 1])
         removeAtomsList = removeAtomsStr.split(';')
+        break
     elif splitArgs[x][0:9] == 'threshold':
         thresholdArg = splitArgs[x].split('=')
         thresholdVal = float(thresholdArg[len(thresholdArg) - 1])
+        break
     elif splitArgs[x][0:12] == 'createAUCpdb':
         aucArg = splitArgs[x].split('=')
         aucVal = aucArg[len(aucArg)-1]
@@ -55,6 +60,7 @@ for x in xrange(1, len(splitArgs)):
             aucVal = False
         else:
             sys.exit('Error 00: Input file is formatted incorrectly\nRead the handbook and amend the input file\n(Try looking at the createAUCpdb argument)')
+        break
     elif splitArgs[x][0:11] == 'createTApdb':
         taArg = splitArgs[x].split("=")
         taVal = taArg[(len(taArg))-1]
@@ -64,8 +70,9 @@ for x in xrange(1, len(splitArgs)):
             taVal = False
         else:
             sys.exit('Error 00: Input file is formatted incorrectly\nRead the handbook and amend the input file\n(Try looking at the createTApdb argument)')
+        break
     else:
         sys.exit('Error 00: Input file is formatted incorrectly\nRead the handbook and amend the input file')
 
 # run fucntion with arguments from input file
-cambda(pathToPDB, PDT=pdtVal, binSize=binVal, addAtoms=addAtomsList, removeAtoms=removeAtomsList, threshold=thresholdVal, createAUCpdb=aucVal, createTApdb=taVal)
+rabdam(pathToPDB, PDT=pdtVal, binSize=binVal, addAtoms=addAtomsList, removeAtoms=removeAtomsList, threshold=thresholdVal, createAUCpdb=aucVal, createTApdb=taVal)
